07db834d7a0e3985e51fc9c64730ab7b4b263a66
Suldin Vyacheslav <suldin_slava.03@mail.ru>
2022-12-05 01:10:29 +0300

    Mypy added

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+from typing import Iterator
+
 import streamlit as st
 
 print("hello")
+
+
+def fib(n: int) -> Iterator[int]:
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a + b
